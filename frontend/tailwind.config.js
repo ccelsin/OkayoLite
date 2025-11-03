d1028843e8aa95e790e2cd6b04a9675cdbd3f128
Maodan ADJAGBONI <124219444+ccelsin@users.noreply.github.com>
2025-11-03 17:42:23 +0100

    Convert frontend to JavaScript

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {}
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        okayo: {
+          primary: "#2563eb",
+          secondary: "#f97316",
+          accent: "#22d3ee",
+          neutral: "#1f2937",
+          "base-100": "#ffffff",
+          info: "#0ea5e9",
+          success: "#22c55e",
+          warning: "#facc15",
+          error: "#ef4444"
+        }
+      }
+    ]
+  }
+};
